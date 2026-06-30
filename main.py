a18926cf246b46d73fcb2d902fd41213170bfcbb
Paulo Henrique <paulohsantosrocha@gmail.com>
2026-06-30 12:54:53 -0300

    feat: add json data insertion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+import json
 
 dt = str(datetime.now()).split()
 
@@ -12,11 +13,21 @@ soup = BeautifulSoup(book_request,"lxml")
 
 books = soup.find_all("article",class_="product_pod")
 
-for book in books:
-    book_title = book.find("h3").text
-    book_price = book.find("p",class_="price_color").text
+data = []
 
+for book in books:
+    book_title = book.find("h3").a.get("title")
+    book_price = book.find("p",class_="price_color").text.strip()
+
+    data.append({
+        "book_name":book_title,
+        "price": book_price
+    })
+    
     print(f"Book Name: {book_title} Price: {book_price}\n")
     print("*"*80)
+    
+with open ("books.json","w") as file:
+    json.dump(data,file,indent=4)
 
 print(f"This scrape was made from {url.replace("https://","")} on {dt[0]} at {dt[1]}")
