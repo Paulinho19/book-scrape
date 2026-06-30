cba546982a43a5d2780fc7943216f487ab703596
Paulo Henrique <paulohsantosrocha@gmail.com>
2026-06-30 15:20:43 -0300

    refactor: embbed system on main function

--- main.py
+++ main.py
@@ -3,31 +3,35 @@ import requests
 from datetime import datetime
 import json
 
-dt = str(datetime.now()).split()
+def main():
+    dt = str(datetime.now()).split()
 
-url = "https://books.toscrape.com"
+    url = "https://books.toscrape.com"
 
-book_request = requests.get(url).text
+    book_request = requests.get(url).text
 
-soup = BeautifulSoup(book_request,"lxml")
+    soup = BeautifulSoup(book_request,"lxml")
 
-books = soup.find_all("article",class_="product_pod")
+    books = soup.find_all("article",class_="product_pod")
 
-data = []
+    data = []
 
-for book in books:
-    book_title = book.find("h3").a.get("title")
-    book_price = book.find("p",class_="price_color").text.strip()
+    for book in books:
+        book_title = book.find("h3").a.get("title")
+        book_price = book.find("p",class_="price_color").text.strip()
 
-    data.append({
-        "book_name":book_title,
-        "price": book_price
-    })
-    
-    print(f"Book Name: {book_title} Price: {book_price}\n")
-    print("*"*80)
-    
-with open ("books.json","w") as file:
-    json.dump(data,file,indent=4)
+        data.append({
+            "book_name":book_title,
+            "price": book_price
+        })
+        
+        print(f"Book Name: {book_title} Price: {book_price}\n")
+        print("*"*80)
+        
+    with open ("books.json","w") as file:
+        json.dump(data,file,indent=4)
 
-print(f"This scrape was made from {url.replace("https://","")} on {dt[0]} at {dt[1]}")
+    print(f"This scrape was made from {url.replace("https://","")} on {dt[0]} at {dt[1]}")
+
+if __name__ == "__main__":
+    main()
